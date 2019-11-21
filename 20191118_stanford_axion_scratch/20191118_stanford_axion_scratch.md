18be9c44ef062e0699395530ccbcdbc97c9ee0a7
Adam Anderson <adama@fnal.gov>
2019-11-20 18:31:10 -0600

    Checkpoint of axion scratch work and adding chi2 normalization.

--- 20191118_stanford_axion_scratch/20191118_stanford_axion_scratch.md
+++ 20191118_stanford_axion_scratch/20191118_stanford_axion_scratch.md
@@ -20,6 +20,8 @@ from spt3g.mapmaker import remove_weight
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+
+from pol_angle import fit_pol_angle, chi2_pol_angle
 ```
 
 Question: How many pixels are in one of Daniel's 2-arcmin pixel maps?
@@ -28,10 +30,6 @@ Question: How many pixels are in one of Daniel's 2-arcmin pixel maps?
 map_fname = '/spt/user/ddutcher/ra0hdec-52.25/y1_ee_20190811/' + \
             'high_150GHz_left_maps/55845637/high_150GHz_left_maps_55845637.g3.gz'
 d_single = list(core.G3File(map_fname))[5]
-```
-
-```python
-print(d_single)
 ```
 
 ```python
@@ -112,6 +110,84 @@ plt.figure(figsize=(12,8))
 plt.imshow(d_corr) #, vmin=-0.1, vmax=0.1)
 # plt.axis([1090,1110,840,860])
 plt.colorbar()
+```
+
+## Tests of Polarization Angle Fitting
+
+
+### Covariance matrix set to identity
+
+
+Check the map-space polarization angle fitting. Let's set the covariance matrix equal to the identity. This will result in nonsensical values of $\chi^2$, but it will be a useful sanity check.
+
+```python
+result = fit_pol_angle(d_single, d_coadd)
+```
+
+```python
+result
+```
+
+```python
+plt.imshow(d_coadd['Q'])
+```
+
+```python
+alpha_plot = np.linspace(-0.5, 0.5, 100)
+
+chi2_plot = np.zeros(len(alpha_plot))
+for jalpha, alpha in enumerate(alpha_plot):
+    chi2_plot[jalpha] = chi2_pol_angle(alpha, d_single, d_coadd, map_covariance='1')
+    print(chi2_plot[jalpha])
+    
+# chi2_plot = [chi2_pol_angle(alpha, d_single, d_coadd, map_covariance='1') for alpha in alpha_plot]
+```
+
+```python
+plt.plot(alpha_plot, chi2_plot)
+```
+
+### Covariance matrix set to rescaled identity
+
+
+The result above is horribly discrepant from zero. This is surely due to the fact that the covariance matrix is just the identity. As a more realistic approximation, let's still assume the covariance matrix is diagonal, but weight by the average noise in a pixel. We can later extend this by using the weights map, but this refinement should only change the chi-square slightly because the pixels on-average have fairly similar weights.
+
+To be precise, our covariance matrix in this test is given by
+\begin{equation}
+\Sigma =
+\begin{pmatrix}
+\textrm{Var}(Q) \times I_n & 0 \\
+0 & \textrm{Var}(U) \times I_n
+\end{pmatrix}.
+\end{equation}
+
+```python
+qvals = np.hstack(np.array(d_single['Q']))
+std_q = np.std(qvals[qvals!=0])
+uvals = np.hstack(np.array(d_single['U']))
+std_u = np.std(uvals[uvals!=0])
+
+result = fit_pol_angle(d_single, d_coadd, 'std', std_q, std_u)
+```
+
+```python
+print(result)
+```
+
+```python
+qvals = np.hstack(np.array(d_single['Q']))
+_ = plt.hist(qvals[qvals!=0],
+         bins=np.linspace(-100,100,101))
+# plt.gca().set_yscale('log')
+```
+
+```python
+alpha_plot = np.linspace(-1.0, 1.0)
+chi2_plot = [chi2_pol_angle(alpha, d_single, d_coadd, map_covariance='1') for alpha in alpha_plot]
+```
+
+```python
+plt.plot(alpha_plot, chi2_plot)
 ```
 
 ```python
--- 20191118_stanford_axion_scratch/pol_angle.py
+++ 20191118_stanford_axion_scratch/pol_angle.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.optimize import minimize
+from spt3g.mapmaker import remove_weight
 
-def chi2_pol_angle(alpha, obs_map_frame, coadd_map_frame, map_covariance='1'):
+def chi2_pol_angle(alpha, obs_map_frame, coadd_map_frame, map_covariance='1',
+                   std_q=None, std_u=None):
     '''
     Chi square used to fit a polarization angle per observation.
 
@@ -16,30 +18,58 @@ def chi2_pol_angle(alpha, obs_map_frame, coadd_map_frame, map_covariance='1'):
     map_covariance : string
         String to select covariance matrix option:
             '1': Identity matrix
+            'std': Identity matrix scaled by the std of the pixels in the map
+               with finite value
         Note that there are too many pixels in the map to pass a true
         covariance matrix, so we need to be clever about how we handle this, 
         hence the options keyed by strings
-    
+    std_q : float
+        The factor by which the Q part of an identity covariance matrix is
+        scaled
+    std_u : float
+        Same as std_q, but applied to the U part of the covariance
+
     Returns
     -------
     chi2 : float
         The value of chi-square evaluated at the given polarization angle
     '''
-    Q = obs_map_frame['Q']
-    U = obs_map_frame['U']
-    Qcoadd = coadd_map_frame['Q']
-    Ucoadd = coadd_map_frame['U']
+    # need to remove weights
+    _, Q, U = remove_weight(obs_map_frame['T'], obs_map_frame['Q'],
+                            obs_map_frame['U'], obs_map_frame['Wpol'])
+    _, Qcoadd, Ucoadd = remove_weight(coadd_map_frame['T'], coadd_map_frame['Q'],
+                                      coadd_map_frame['U'], coadd_map_frame['Wpol'])
 
+    # different heuristic choices of covariance matrix
     if map_covariance == '1':
         Qresidual = Q - (1/2)*np.cos(2*alpha)*Qcoadd + (1/2)*np.sin(2*alpha)*Ucoadd
         Uresidual = U - (1/2)*np.cos(2*alpha)*Ucoadd - (1/2)*np.sin(2*alpha)*Qcoadd
+        Qresidual_arr = np.array(Qresidual)
+        Qresidual_arr = Qresidual_arr[np.isfinite(Qresidual_arr)]
+        Uresidual_arr = np.array(Uresidual)
+        Uresidual_arr = Uresidual_arr[np.isfinite(Uresidual_arr)]
 
-        chi2 = np.sum(Qresidual * Qresidual) + np.sum(Uresidual * Uresidual)
+        chi2 = np.sum(Qresidual_arr * Qresidual_arr) + np.sum(Uresidual_arr * Uresidual_arr)
+
+    if map_covariance == 'std':
+        if std_q is None or std_u is None:
+            raise ValueError('Argument `map_covariance` set to `std`, but '
+                             '`std_q` and/or `std_u` not specified.')
+        Qresidual = Q - (1/2)*np.cos(2*alpha)*Qcoadd + (1/2)*np.sin(2*alpha)*Ucoadd
+        Uresidual = U - (1/2)*np.cos(2*alpha)*Ucoadd - (1/2)*np.sin(2*alpha)*Qcoadd
+        Qresidual_arr = np.array(Qresidual)
+        Qresidual_arr = Qresidual_arr[np.isfinite(Qresidual_arr)]
+        Uresidual_arr = np.array(Uresidual)
+        Uresidual_arr = Uresidual_arr[np.isfinite(Uresidual_arr)]
+
+        chi2 = np.sum(Qresidual_arr * Qresidual_arr) / (std_q**2) + \
+               np.sum(Uresidual_arr * Uresidual_arr) / (std_u**2)
 
     return chi2
 
 
-def fit_pol_angle(obs_map_frame, coadd_map_frame, map_covariance='1'):
+def fit_pol_angle(obs_map_frame, coadd_map_frame, map_covariance='1',
+                  std_q=None, std_u=None):
     '''
     Fit for the polarization angle for a single observation by minimizing
     the map-space chi-square.
@@ -63,5 +93,6 @@ def fit_pol_angle(obs_map_frame, coadd_map_frame, map_covariance='1'):
         The value of chi-square evaluated at the given polarization angle
     '''
     result = minimize(chi2_pol_angle, 0, args=(obs_map_frame, coadd_map_frame,
-                                               map_covariance), method='Powell')
+                                               map_covariance, std_q, std_u),
+                      method='Powell')
     return result
