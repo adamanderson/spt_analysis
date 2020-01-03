935eb00d4eae7e864aa50aea83c9fbf41422ed6b
Adam Anderson <adama@fnal.gov>
2020-01-03 16:33:55 -0600

    Add timeseries analysis to axion sensitivity code.

--- 20190811_axion_rotation/axion_sensitivity_eb.py
+++ 20190811_axion_rotation/axion_sensitivity_eb.py
@@ -11,15 +11,42 @@ P0 = ap.ArgumentParser(description='Script for estimating the sensitivity of '
 S0 = P0.add_subparsers(dest='mode', metavar='MODE', title='subcommands',
                        help='Function to perform. For help, call: '
                        '%(prog)s %(metavar)s -h')
+
 SP0 = S0.add_parser('singlemap', help='Simulate a single map and estimation of '
                    'its polarization angle.',
                    formatter_class=ap.ArgumentDefaultsHelpFormatter)
-SP0.add_argument('--noise-per-map', action='store', type=float, default=150,
+SP0.add_argument('--noise-per-obs', action='store', type=float, default=150,
                  help='Noise level per map in units of [uK-arcmin].')
 SP0.add_argument('--n-sims', action='store', type=int, default=2000,
                  help='Number of simulated Cls to generate.')
 SP0.add_argument('--field-size', action='store', type=int, default=500,
                  help='Size of field in square degrees.')
+SP0.add_argument('--lmin', action='store', type=int, default=100,
+                 help='lmin to use for analysis.')
+SP0.add_argument('--lmax', action='store', type=int, default=2000,
+                 help='lmax to use for analysis.')
+SP0.add_argument('--cosmic-variance', action='store_true', 
+                 help='Include cosmic variance..')
+
+SP1 = S0.add_parser('timeseries', help='Simulate data as a function of time '
+                    'and optionally fit it to a sinusoid.',
+                    formatter_class=ap.ArgumentDefaultsHelpFormatter)
+SP1.add_argument('--noise-per-obs', action='store', type=float, default=150,
+                 help='Noise level per map in units of [uK-arcmin].')
+SP1.add_argument('--field-size', action='store', type=int, default=500,
+                 help='Size of field in square degrees.')
+SP1.add_argument('--n-bundles', action='store', type=int, default=1000,
+                 help='Number observations to simulate.')
+SP1.add_argument('--n-sims', action='store', type=int, default=1000,
+                 help='Number of simulations to use for covariance estimation.')
+SP1.add_argument('--obs-per-bundle', action='store', type=int, default=1,
+                 help='Coadd N observations into bundles, and then fit for '
+                 'a polarization angle for each bundle.')
+SP1.add_argument('--lmin', action='store', type=int, default=100,
+                 help='lmin to use for analysis.')
+SP1.add_argument('--lmax', action='store', type=int, default=3000,
+                 help='lmax to use for analysis.')
+
 args = P0.parse_args()
 
 
@@ -43,7 +70,7 @@ Cl_theory = {'TT': unlensedCL[1:,0],
 
 
 def generate_Cl(spectrum, ells, Cl_theory, f_sky, lmin, lmax, map_noise_T,
-                beam_fwhm):
+                beam_fwhm, cosmic_variance=False):
     '''
     Generate a Gaussian realization of the Cl's from a cosmology.
 
@@ -82,23 +109,37 @@ def generate_Cl(spectrum, ells, Cl_theory, f_sky, lmin, lmax, map_noise_T,
     sigma_beam = beam_fwhm / np.sqrt(8*np.log(2.)) * np.pi / 10800
     beam_weight = np.exp(-1.*l_range**2. * sigma_beam**2.)
     
-    if spectrum == 'TE':
-        var_factor = (Cl_theory['TE'][ells_cut]**2 + \
-                      (Cl_theory['TT'][ells_cut] + noise_weight_T * beam_weight) * \
-                      (Cl_theory['EE'][ells_cut] + noise_weight_P * beam_weight))
-        Cl_mean = Cl_theory['TE'][ells_cut]
-    elif spectrum == 'EB':
-        var_factor = ((Cl_theory['BB'][ells_cut] + noise_weight_P * beam_weight) * \
-                      (Cl_theory['EE'][ells_cut] + noise_weight_P * beam_weight))
-        Cl_mean = np.zeros(Cl_theory['TT'][ells_cut].shape)
-    elif spectrum == 'TB':
-        var_factor = ((Cl_theory['TT'][ells_cut] + noise_weight_T * beam_weight) * \
-                      (Cl_theory['BB'][ells_cut] + noise_weight_P * beam_weight))
-        Cl_mean = np.zeros(Cl_theory['TT'][ells_cut].shape)
+    if cosmic_variance:
+        if spectrum == 'TE':
+            var_factor = (Cl_theory['TE'][ells_cut]**2 + \
+                          (Cl_theory['TT'][ells_cut] + noise_weight_T * beam_weight) * \
+                          (Cl_theory['EE'][ells_cut] + noise_weight_P * beam_weight))
+            Cl_mean = Cl_theory['TE'][ells_cut]
+        elif spectrum == 'EB':
+            var_factor = ((Cl_theory['BB'][ells_cut] + noise_weight_P * beam_weight) * \
+                          (Cl_theory['EE'][ells_cut] + noise_weight_P * beam_weight))
+            Cl_mean = np.zeros(Cl_theory['TT'][ells_cut].shape)
+        elif spectrum == 'TB':
+            var_factor = ((Cl_theory['TT'][ells_cut] + noise_weight_T * beam_weight) * \
+                          (Cl_theory['BB'][ells_cut] + noise_weight_P * beam_weight))
+            Cl_mean = np.zeros(Cl_theory['TT'][ells_cut].shape)
+        else:
+            var_factor = (Cl_theory[spectrum][ells_cut] + noise_weight_P * beam_weight)**2
+            Cl_mean = Cl_theory[spectrum][ells_cut]
     else:
-        var_factor = (Cl_theory[spectrum][ells_cut] + noise_weight_P * beam_weight)**2
-        Cl_mean = Cl_theory[spectrum][ells_cut]
-    
+        if spectrum == 'TE':
+            var_factor = (noise_weight_T * beam_weight) * (noise_weight_P * beam_weight)
+            Cl_mean = Cl_theory['TE'][ells_cut]
+        elif spectrum == 'EB':
+            var_factor = (noise_weight_P * beam_weight) * (noise_weight_P * beam_weight)
+            Cl_mean = np.zeros(Cl_theory['TT'][ells_cut].shape)
+        elif spectrum == 'TB':
+            var_factor = (noise_weight_T * beam_weight) * (noise_weight_P * beam_weight)
+            Cl_mean = np.zeros(Cl_theory['TT'][ells_cut].shape)
+        else:
+            var_factor = (noise_weight_P * beam_weight)**2
+            Cl_mean = Cl_theory[spectrum][ells_cut]
+
     Cl_sigma = np.sqrt(2 / ((2*l_range + 1)*f_sky) * var_factor)
     
     # generate a gaussian realization of all the Cl's, then
@@ -197,60 +238,83 @@ def pol_chi2(x, Cl_TB, Cl_TE, Cl_EB, Cl_EE, Cl_BB, var_Dl_TB, var_Dl_EB):
     chi2_array = np.array(chi2_array)
     return chi2_array
 
-def delta_chi2_plus1(x, Cl_TB, Cl_TE, Cl_EB, Cl_EE, Cl_BB, var_Dl_TB, var_Dl_EB, func_min):
-    return pol_chi2(x, Cl_TB, Cl_TE, Cl_EB, Cl_EE, Cl_BB, var_Dl_TB, var_Dl_EB) - func_min - 1
 
+def delta_chi2_plus1(x, Cl_TB, Cl_TE, Cl_EB, Cl_EE, Cl_BB, var_Dl_TB,
+                     var_Dl_EB, func_min):
+    return pol_chi2(x, Cl_TB, Cl_TE, Cl_EB, Cl_EE, Cl_BB, var_Dl_TB, var_Dl_EB) - \
+        func_min - 1
+
+
+# simulated maps for either the single-map mode or for
+# covariance estimation in the time=series mode
+Cl_random = {}
+for spectrum in ['TB', 'TE', 'EB', 'EE', 'BB']:
+    Cl_random[spectrum] = np.zeros(args.n_sims)
 
 if args.mode == 'singlemap':
-    delta_chi2_sims = np.zeros(args.n_sims)
-    angle_fit = np.zeros(args.n_sims)
-    angle_up1sigma = np.zeros(args.n_sims)
-    angle_down1sigma = np.zeros(args.n_sims)
-
-    Cl_random = {}
-    for spectrum in ['TB', 'TE', 'EB', 'EE', 'BB']:
-        Cl_random[spectrum] = np.zeros(args.n_sims)
-    Dl_sims = np.zeros(args.n_sims)
-
-
     # generate the simulated spectra
     for jsim in range(args.n_sims):
         for spectrum in Cl_random:
             Cl_random[spectrum][jsim] = generate_Cl_averaged(spectrum, ells,
                                                              Cl_theory,
                                                              f_sky=args.field_size/41000,
-                                                             lmin=100, lmax=3000,
-                                                             map_noise_T=args.noise_per_map,
+                                                             lmin=args.lmin, lmax=args.lmax,
+                                                             map_noise_T=args.noise_per_obs,
                                                              beam_fwhm=1.2)
 
-    # minimize the chi-square and estimate the polarizationg angle and uncertainty
+    chi2_sims = np.zeros(args.n_sims)
+    delta_chi2_sims = np.zeros(args.n_sims)
+    angle_fit = np.zeros(args.n_sims)
+    angle_up1sigma = np.zeros(args.n_sims)
+    angle_down1sigma = np.zeros(args.n_sims)
+
+    # minimize the chi-square and estimate the polarization angle and uncertainty
     for jsim in range(args.n_sims):
-        out = minimize(pol_chi2, 0, args=(Cl_random['TB'][jsim], Cl_random['TE'][jsim],
-                                          Cl_random['EB'][jsim], Cl_random['EE'][jsim],
+        out = minimize(pol_chi2, 0, args=(Cl_random['TB'][jsim],
+                                          Cl_random['TE'][jsim],
+                                          Cl_random['EB'][jsim],
+                                          Cl_random['EE'][jsim],
                                           Cl_random['BB'][jsim],
-                                          np.var(Cl_random['TB']), np.var(Cl_random['EB'])),
+                                          np.var(Cl_random['TB']),
+                                          np.var(Cl_random['EB'])),
                        method='Powell')
         angle_fit[jsim] = out.x
-        Dl_sims[jsim] = out.fun
+        chi2_sims[jsim] = out.fun
 
         try:
-            delta_chi2_sims[jsim]  = pol_chi2(0, Cl_random['TB'][jsim], Cl_random['TE'][jsim],
-                                              Cl_random['EB'][jsim], Cl_random['EE'][jsim],
+            delta_chi2_sims[jsim]  = pol_chi2(0,
+                                              Cl_random['TB'][jsim],
+                                              Cl_random['TE'][jsim],
+                                              Cl_random['EB'][jsim],
+                                              Cl_random['EE'][jsim],
                                               Cl_random['BB'][jsim],
-                                              np.var(Cl_random['TB']), np.var(Cl_random['EB'])) - Dl_sims[jsim]
-            angle_up1sigma[jsim]   = newton(delta_chi2_plus1, x0=angle_fit[jsim] + 0.01,
-                                            args=(Cl_random['TB'][jsim], Cl_random['TE'][jsim],
-                                                  Cl_random['EB'][jsim], Cl_random['EE'][jsim], Cl_random['BB'][jsim],
-                                                  np.var(Cl_random['TB']), np.var(Cl_random['EB']), Dl_sims[jsim]))
-            angle_down1sigma[jsim] = newton(delta_chi2_plus1, x0=angle_fit[jsim] - 0.01,
-                                            args=(Cl_random['TB'][jsim], Cl_random['TE'][jsim],
-                                                  Cl_random['EB'][jsim], Cl_random['EE'][jsim], Cl_random['BB'][jsim],
-                                                  np.var(Cl_random['TB']), np.var(Cl_random['EB']), Dl_sims[jsim]))
+                                              np.var(Cl_random['TB']),
+                                              np.var(Cl_random['EB'])) - chi2_sims[jsim]
+            angle_up1sigma[jsim]   = newton(delta_chi2_plus1,
+                                            x0=angle_fit[jsim] + 0.01,
+                                            args=(Cl_random['TB'][jsim],
+                                                  Cl_random['TE'][jsim],
+                                                  Cl_random['EB'][jsim],
+                                                  Cl_random['EE'][jsim],
+                                                  Cl_random['BB'][jsim],
+                                                  np.var(Cl_random['TB']),
+                                                  np.var(Cl_random['EB']),
+                                                  chi2_sims[jsim]))
+            angle_down1sigma[jsim] = newton(delta_chi2_plus1,
+                                            x0=angle_fit[jsim] - 0.01,
+                                            args=(Cl_random['TB'][jsim],
+                                                  Cl_random['TE'][jsim],
+                                                  Cl_random['EB'][jsim],
+                                                  Cl_random['EE'][jsim],
+                                                  Cl_random['BB'][jsim],
+                                                  np.var(Cl_random['TB']),
+                                                  np.var(Cl_random['EB']),
+                                                  chi2_sims[jsim]))
         except RuntimeError:
             print('Failed to estimate error bar. Skipping simulation.')
 
     plt.figure(1)
-    hist_range = np.std(angle_up1sigma) * 4 * 180/np.pi
+    hist_range = 10 #np.std(angle_up1sigma) * 4 * 180/np.pi
     _ = plt.hist(angle_up1sigma * 180/np.pi, histtype='step',
                  bins=np.linspace(-1*hist_range,hist_range,51),
                  label='$1\sigma$ upper limit')
@@ -265,3 +329,91 @@ if args.mode == 'singlemap':
     plt.ylabel('number of simulations')
     plt.tight_layout()
     plt.savefig('angle_hist.png', dpi=150)
+
+elif args.mode == 'timeseries':
+    chi2_sims = np.zeros(args.n_bundles)
+    delta_chi2_sims = np.zeros(args.n_bundles)
+    angle_fit = np.zeros(args.n_bundles)
+    angle_up1sigma = np.zeros(args.n_bundles)
+    angle_down1sigma = np.zeros(args.n_bundles)
+
+    Cl_per_bundle = {}
+    for spectrum in ['TB', 'TE', 'EB', 'EE', 'BB']:
+        Cl_per_bundle[spectrum] = np.zeros(args.n_bundles)
+    noise_per_bundle = args.noise_per_obs / np.sqrt(args.obs_per_bundle)
+
+    # generate simulations to compute the covariance
+    for jsim in range(args.n_sims):
+        for spectrum in Cl_random:
+            Cl_random[spectrum][jsim] = generate_Cl_averaged(spectrum, ells,
+                                                             Cl_theory,
+                                                             f_sky=args.field_size/41000,
+                                                             lmin=args.lmin, lmax=args.lmax,
+                                                             map_noise_T=noise_per_bundle,
+                                                             beam_fwhm=1.2)
+
+
+    # generate the simulated spectra
+    for jbundle in range(args.n_bundles):
+        for spectrum in Cl_per_bundle:
+            Cl_per_bundle[spectrum][jbundle] = generate_Cl_averaged(spectrum, ells,
+                                                                    Cl_theory,
+                                                                    f_sky=args.field_size/41000,
+                                                                    lmin=args.lmin, lmax=args.lmax,
+                                                                    map_noise_T=noise_per_bundle,
+                                                                    beam_fwhm=1.2)
+        out = minimize(pol_chi2, 0, args=(Cl_per_bundle['TB'][jbundle],
+                                          Cl_per_bundle['TE'][jbundle],
+                                          Cl_per_bundle['EB'][jbundle],
+                                          Cl_per_bundle['EE'][jbundle],
+                                          Cl_per_bundle['BB'][jbundle],
+                                          np.var(Cl_random['TB']),
+                                          np.var(Cl_random['EB'])), method='Powell')
+        angle_fit[jbundle] = out.x
+        chi2_sims[jbundle] = out.fun
+
+        angle_up1sigma[jbundle] = newton(delta_chi2_plus1,
+                                         x0=angle_fit[jbundle] + 0.01,
+                                         args=(Cl_per_bundle['TB'][jbundle],
+                                               Cl_per_bundle['TE'][jbundle],
+                                               Cl_per_bundle['EB'][jbundle],
+                                               Cl_per_bundle['EE'][jbundle],
+                                               Cl_per_bundle['BB'][jbundle],
+                                               np.var(Cl_random['TB']),
+                                               np.var(Cl_random['EB']),
+                                               chi2_sims[jbundle]))
+        angle_down1sigma[jbundle] = newton(delta_chi2_plus1,
+                                           x0=angle_fit[jbundle] - 0.01,
+                                           args=(Cl_per_bundle['TB'][jbundle],
+                                                 Cl_per_bundle['TE'][jbundle],
+                                                 Cl_per_bundle['EB'][jbundle],
+                                                 Cl_per_bundle['EE'][jbundle],
+                                                 Cl_per_bundle['BB'][jbundle],
+                                                 np.var(Cl_random['TB']),
+                                                 np.var(Cl_random['EB']),
+                                                 chi2_sims[jbundle]))
+
+    # symmetrize the error
+    angle_error_per_obs = (angle_up1sigma - angle_down1sigma) / 2
+
+    # The minimizer sometimes fails and best fit ends up at N*pi/4 away from zero.
+    # For now, just throw these values away.
+    # TODO: Do something sensible with these values.
+    angle_fit_clipped, clip_lower, clip_upper = sigmaclip(angle_fit, low=3, high=3)
+    angle_error_per_obs_clipped = angle_error_per_obs[(angle_fit>clip_lower) & \
+                                                      (angle_fit<clip_upper)]
+    angle_fit = angle_fit_clipped
+    angle_error_per_obs = angle_error_per_obs_clipped
+
+    
+    plt.figure(2)
+    _ = plt.errorbar(np.arange(len(angle_fit)), angle_fit * 180/np.pi,
+                     yerr=angle_error_per_obs * 180/np.pi,
+                     linestyle='None', marker='o', markersize=3)
+    plt.xlabel('bundle index')
+    plt.ylabel('rotation angle [deg]')
+    plt.title('simulated global rotation angle\n'
+              '{} observations / bundle, {:.1f} uK-arcmin per bundle\n'\
+              .format(args.obs_per_bundle, noise_per_bundle))
+    plt.tight_layout()
+    plt.savefig('angle_vs_bundle.png', dpi=150)
