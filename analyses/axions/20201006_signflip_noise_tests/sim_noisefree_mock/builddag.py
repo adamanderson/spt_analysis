e2f31d1771c7bd6386739788c450ee573e7605af
Adam Anderson <adama@fnal.gov>
2020-11-10 21:25:21 -0600

    Add ability to run over multiple simulated skies to script that generates dag for mock-observing.

--- analyses/axions/20201006_signflip_noise_tests/sim_noisefree_mock/builddag.py
+++ analyses/axions/20201006_signflip_noise_tests/sim_noisefree_mock/builddag.py
@@ -2,23 +2,35 @@ from glob import glob
 import os
 from spt3g import core, std_processing
 
-fitsflag = '-m /sptgrid/user/adama/20201006_signflip_noise_tests/total/total_150ghz_map_3g_0000.fits'
+nskies = 5
+fitsflag = '-m /sptgrid/user/adama/20201006_signflip_noise_tests_2/total/total_150ghz_map_3g_0000.fits'
 yamlfile = 'axion_noisefree_mock_july2019_params.yaml'
 ptsrcfile = '1500d_ptsrc_and_decrement_list.txt'
-logdir = '/scratch/adama/condor_logs/20201006_signflip_noise_tests'
+logdir = '/scratch/adama/condor_logs/20201006_signflip_noise_tests_2'
 mapmaker = 'master_field_mapmaker_signflip.py'
 otherfiles = 'axiontod.py'
+equalweights_equalflags = True
 
 simstub_fnames = glob('/sptgrid/user/kferguson/axion_perscan_maps_2019/simstub_*_150GHz*g3.gz')
+if equalweights_equalflags:
+    simstub_fnames = [fname for fname in simstub_fnames \
+                      if 'no_equal_weights_equal_flags' not in fname]
+else:
+    simstub_fnames = [fname for fname in simstub_fnames \
+                      if 'no_equal_weights_equal_flags' in fname]
+
 for fname in simstub_fnames:
-    jobstr = os.path.basename(fname).lstrip('simstub_')[:-6]
-    print('JOB noisefree-mock-sims-{} makemanymaps.submit'.format(jobstr))
-    print('VARS noisefree-mock-sims-{} InputFiles=\"{}\"'.format(jobstr, fname))
-    print('VARS noisefree-mock-sims-{} OutputFiles=\"/sptgrid/user/adama/20201006_signflip_noise_tests/noisefree-mock-sims-{}.g3.gz\"'.format(jobstr, jobstr))
-    print('VARS noisefree-mock-sims-{} YAMLFile=\"{}\"'.format(jobstr, yamlfile))
-    print('VARS noisefree-mock-sims-{} PtSrcFile=\"{}\"'.format(jobstr, ptsrcfile))
-    print('VARS noisefree-mock-sims-{} LogDir=\"{}\"'.format(jobstr, logdir))
-    print('VARS noisefree-mock-sims-{} Mapmaker=\"{}\"'.format(jobstr, mapmaker))
-    print('VARS noisefree-mock-sims-{} OtherFiles=\"{}\"'.format(jobstr, otherfiles))
-    print('VARS noisefree-mock-sims-{} ExtraArgs=\"--sim --config-file {} -z {}\"'.format(jobstr, yamlfile, fitsflag))
-    print('VARS noisefree-mock-sims-{} JobID=\"noisefree-mock-sims-{}\"'.format(jobstr, jobstr))
+    for jsky in range(nskies):
+        jobstr = '{}_{:04d}'.format(os.path.basename(fname).lstrip('simstub_')[:-6], jsky)
+        fitsflag = '-m /sptgrid/user/adama/20201006_signflip_noise_tests_2/mock_skies/total/total_150ghz_map_3g_{:04d}.fits'.format(jsky)
+
+        print('JOB noisefree-mock-sims-{} makemanymaps.submit'.format(jobstr))
+        print('VARS noisefree-mock-sims-{} InputFiles=\"{}\"'.format(jobstr, fname))
+        print('VARS noisefree-mock-sims-{} OutputFiles=\"/sptgrid/user/adama/20201006_signflip_noise_tests_2/noisefree-mock-sims-{}.g3.gz\"'.format(jobstr, jobstr))
+        print('VARS noisefree-mock-sims-{} YAMLFile=\"{}\"'.format(jobstr, yamlfile))
+        print('VARS noisefree-mock-sims-{} PtSrcFile=\"{}\"'.format(jobstr, ptsrcfile))
+        print('VARS noisefree-mock-sims-{} LogDir=\"{}\"'.format(jobstr, logdir))
+        print('VARS noisefree-mock-sims-{} Mapmaker=\"{}\"'.format(jobstr, mapmaker))
+        print('VARS noisefree-mock-sims-{} OtherFiles=\"{}\"'.format(jobstr, otherfiles))
+        print('VARS noisefree-mock-sims-{} ExtraArgs=\"--sim --config-file {} -z {}\"'.format(jobstr, yamlfile, fitsflag))
+        print('VARS noisefree-mock-sims-{} JobID=\"noisefree-mock-sims-{}\"'.format(jobstr, jobstr))
